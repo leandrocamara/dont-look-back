629c9ed247c1db076a588e31e4e1ccbb8ec11e70
Leandro da Silva Câmara <leandro@basekids.com.br>
2019-03-22 11:26:54 -0300

    UPATE - Change in the Environment

---
+++ vr-treasure-hunt/Assets/Scripts/FPS.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    private Text fps;
+    private float deltaTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fps = GetComponent<Text>();
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        fps.text = "FPS: " + (int) (1.0f / deltaTime);
+    }
+
+    public void DisableDungeon(GameObject dungeon)
+    {
+        dungeon.SetActive(!dungeon.active);
+    }
+}
