6fd0eec5db0d7c80252772855d6b75a5b82d98ce
Leandro da Silva Câmara <leandro@basekids.com.br>
2019-03-29 14:26:16 -0300

    ADD - Animation of the Chests

---
+++ vr-treasure-hunt/Assets/Scripts/Chest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Animator anim;
+    public MonsterController monsterController;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void OpenChest()
+    {
+        anim.SetTrigger("Open");
+    }
+
+    public void TakeGoldChest()
+    {
+        monsterController.ChasePlayer();
+    }
+}
--- vr-treasure-hunt/Assets/Scripts/Controller/MonsterController.cs
+++ vr-treasure-hunt/Assets/Scripts/Controller/MonsterController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     public GameObject monsterStatic;
+    public GameObject monsterAlive;
     public AudioSource audioMonsterStatic;
+    public AudioSource audioMonsterAlive;
+    public Door[] doorsToClose;
+    public Door[] doorsToOpen;
 
     private void Start()
     {
+        monsterAlive.SetActive(false);
         monsterStatic.SetActive(false);
     }
 
@@ -16,5 +21,30 @@ public class MonsterController : MonoBehaviour
     {
         audioMonsterStatic.Play();
         monsterStatic.SetActive(true);
+    }
+
+    public void ChasePlayer()
+    {
+        audioMonsterAlive.Play();
+        monsterAlive.SetActive(true);
+
+        OpenDoorsExitRoomChest();
+        CloseDoorsExitRoomChest();
+    }
+
+    private void OpenDoorsExitRoomChest()
+    {
+        foreach (var door in doorsToOpen)
+        {
+            door.OpenDoor();
+        }
+    }
+
+    private void CloseDoorsExitRoomChest()
+    {
+        foreach (var door in doorsToClose)
+        {
+            door.CloseDoor();
+        }
     }
 }
